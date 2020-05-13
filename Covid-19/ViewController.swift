9bfd7e6e86b0811f8f0fdfdc98eb8b0ae3175f94
Mike Dmitriev <misha.dmitriev.spb@gmail.com>
2020-05-13 19:37:38 +0300

    Initial

---
+++ Covid-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19
+//
+//  Created by Михаил Дмитриев on 13.05.2020.
+//  Copyright © 2020 Mikhail Dmitriev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
